cb96353d13ec213c6a6c6fe20f6b5e530e21c8e4
Adam Wright <adam@adamjwright.com>
2020-02-04 03:05:24 -0800

    Create initial python translation

---
+++ 5- random_testing/testme.py
@@ -0,0 +1,54 @@
+# Author:       Adam Wright
+# Date:         2-4-2020
+# Description:  Python re-implementation of testme.c file
+
+
+import random
+
+
+def inputChar():
+    return ' '
+
+
+def inputString():
+    return ""
+
+
+def testme():
+    tcCount = 0
+    s = ""
+    c = ''
+    state = 0
+
+    while True:
+        tcCount+=1
+        test_char = inputChar()
+        test_string = inputString()
+        print("Iteration " + tcCount + "c = " + test_char + ", " + "s = " + test_string + ", " + "state = " + state)
+
+        if c == '[' and state == 0:
+            state = 1
+        if c == '(' and state == 1:
+            state = 2
+        if c == '{' and state == 2:
+            state = 3
+        if c == ' ' and state == 3:
+            state = 4
+        if c == 'a' and state == 4:
+            state = 5
+        if c == 'x' and state == 5:
+            state = 6
+        if c == '}' and state == 6:
+            state = 7
+        if c == ')' and state == 7:
+            state = 8
+        if c == ']' and state == 8:
+            state = 9
+        if state == 9:
+            print("error ")
+            exit(200)
+
+
+def main():
+    random.seed(None)
+    testme()
